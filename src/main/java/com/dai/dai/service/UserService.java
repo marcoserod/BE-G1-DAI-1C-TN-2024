62a4933e504f0f414b65dd767883c512f900daa3
Juan Manuel Nugnes <juanmanuelnugnes@gmail.com>
2024-04-16 16:51:32 -0300

    Added GET favorites endpoint

--- src/main/java/com/dai/dai/service/UserService.java
+++ src/main/java/com/dai/dai/service/UserService.java
@@ -1,11 +1,10 @@
 package com.dai.dai.service;
 
+import com.dai.dai.dto.movie.response.GetMoviesResponse;
 import com.dai.dai.dto.user.dto.UserDto;
 import com.dai.dai.dto.user.dto.UserFavoriteDto;
-import com.dai.dai.entity.UserFavoriteEntity;
 
 import java.io.IOException;
-import java.util.List;
 
 public interface UserService {
     UserDto getUserInfoById(Integer userID);
@@ -13,4 +12,6 @@ public interface UserService {
     UserDto createUser(UserDto userDto);
 
     void addFavorite(UserFavoriteDto userFavoriteDto) throws IOException, InterruptedException;
+
+    GetMoviesResponse getFavorites(Integer userID) throws IOException, InterruptedException;
 }
